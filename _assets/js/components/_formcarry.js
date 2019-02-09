bd10f78eecd20a128f0b6dcc0433b6eb4dd9d17d
thomasvaeth <thomas.vaeth@gmail.com>
2019-02-09 09:51:22 -0800

    Made all form fields required.

--- _assets/js/components/_formcarry.js
+++ _assets/js/components/_formcarry.js
@@ -12,11 +12,16 @@ const Formcarry = (() => {
         form: document.querySelector('.form'),
         formAction: document.querySelector('.form').action,
         formSubmit: document.querySelector('.form .btn'),
-        formMessage: document.querySelector('.form__message'),
+        formError: document.querySelector('.form__error'),
+        formFirstName: document.getElementById('form-first-name'),
+        formLastName: document.getElementById('form-last-name'),
+        formEmail: document.getElementById('form-email'),
+        formMessage: document.getElementById('form-message'),
         animation: 'fade-in',
         open: 'js-popup-open',
         overflow: 'js-overflow',
-        closing: 'js-popup-closing'
+        closing: 'js-popup-closing',
+        error: 'js-error'
       };
     },
 
@@ -33,33 +38,68 @@ const Formcarry = (() => {
       s.formSubmit.addEventListener('click', e => {
         e.preventDefault();
 
-        const request = new XMLHttpRequest();
+        if (s.form.checkValidity()) {
+          s.formFirstName.classList.remove(s.error);
+          s.formLastName.classList.remove(s.error);
+          s.formEmail.classList.remove(s.error);
+          s.formMessage.classList.remove(s.error);
 
-        request.open('POST', s.formAction, true);
-        request.setRequestHeader('accept', 'application/json');
+          const request = new XMLHttpRequest();
 
-        const formData = new FormData(s.form);
+          request.open('POST', s.formAction, true);
+          request.setRequestHeader('accept', 'application/json');
 
-        request.send(formData);
+          const formData = new FormData(s.form);
 
-        request.onreadystatechange = () => {
-          if (request.readyState === 4 && request.status === 200) {
-            s.body.classList.add(s.closing);
-            s.body.classList.remove(s.open);
-            s.html.classList.remove(s.overflow);
+          request.send(formData);
 
-            setTimeout(() => {
-              s.form.reset();
-              s.body.classList.remove(s.closing);
-            }, 800);
+          request.onreadystatechange = () => {
+            if (request.readyState === 4 && request.status === 200) {
+              s.body.classList.add(s.closing);
+              s.body.classList.remove(s.open);
+              s.html.classList.remove(s.overflow);
+
+              setTimeout(() => {
+                s.form.reset();
+                s.body.classList.remove(s.closing);
+              }, 800);
+            } else {
+              setTimeout(() => {
+                s.formError.classList.remove(s.animation);
+                s.formError.classList.add(s.animation);
+                s.formError.textContent = 'Something Went Wrong';
+              }, 750);
+            }
+          };
+        } else {
+          if (!s.formFirstName.checkValidity()) {
+            s.formFirstName.classList.add(s.error);
           } else {
-            setTimeout(() => {
-              s.formMessage.classList.remove(s.animation);
-              s.formMessage.classList.add(s.animation);
-              s.formMessage.textContent = 'Something Went Wrong';
-            }, 750);
+            s.formFirstName.classList.remove(s.error);
           }
-        };
+
+          if (!s.formLastName.checkValidity()) {
+            s.formLastName.classList.add(s.error);
+          } else {
+            s.formLastName.classList.remove(s.error);
+          }
+
+          if (!s.formEmail.checkValidity()) {
+            s.formEmail.classList.add(s.error);
+          } else {
+            s.formEmail.classList.remove(s.error);
+          }
+
+          if (!s.formMessage.checkValidity()) {
+            s.formMessage.classList.add(s.error);
+          } else {
+            s.formMessage.classList.remove(s.error);
+          }
+
+          s.formError.classList.remove(s.animation);
+          s.formError.classList.add(s.animation);
+          s.formError.textContent = 'All Fields Required';
+        }
       });
     }
   };
